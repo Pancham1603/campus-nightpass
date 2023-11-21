ade8482a02a878f1686911ab25d30e332db7c13f
Pancham Agarwal <pancham1603@gmail.com>
2023-11-21 15:20:49 +0530

    added scanner manual input for physical reader

--- apps/validation/static/info.js
+++ apps/validation/static/info.js
@@ -1,3 +1,34 @@
+
+function fetch_data(dump) {
+    urls = ""
+    $.ajax({
+    method: "POST",
+    url: urls,
+    data:dump,
+    dataType: "json",
+    timeout: 120000,
+    success: function (response) {
+        let res = response.status;
+        if (res) {
+            updateProfile(response.user);
+            updateUserPass(response.user_pass, response.user, response.task);
+            if (response.user_pass.pass_id) {
+                document.getElementById('pass-card').style.backgroundColor = '#90EE90'
+            } else [
+            document.getElementById('pass-card').style.backgroundColor = '#FF7F7F'
+            ]
+            toastr.success(response.message);
+        }
+        else {
+            toastr.error(response.message);
+        }
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        toastr.error("Something went wrong, please try again later");
+    }
+});
+}
+
 function updateProfile(data) {
     const profileDetails = document.querySelector('.profile-details');
     const nameElement = profileDetails.querySelector('h2');
@@ -48,36 +79,6 @@ function updateUserPass(data,user_data, task) {
             actionButton.style.visibility = 'visible';
             actionButton.innerHTML = `Check In`;
             actionButton.onclick = function(){checkIn(user_data.registration_number);}}
-}
-
-function fetch_data(dump) {
-    urls = ""
-    $.ajax({
-    method: "POST",
-    url: urls,
-    data:dump,
-    dataType: "json",
-    timeout: 120000,
-    success: function (response) {
-        let res = response.status;
-        if (res) {
-            updateProfile(response.user);
-            updateUserPass(response.user_pass, response.user, response.task);
-            if (response.user_pass.pass_id) {
-                document.getElementById('pass-card').style.backgroundColor = '#90EE90'
-            } else [
-            document.getElementById('pass-card').style.backgroundColor = '#FF7F7F'
-            ]
-            toastr.success(response.message);
-        }
-        else {
-            toastr.error(response.message);
-        }
-    },
-    error: function (xhr, textStatus, errorThrown) {
-        toastr.error("Something went wrong, please try again later");
-    }
-});
 }
 
 function checkIn(registration_number) {
@@ -131,3 +132,25 @@ function checkOut(registration_number) {
     }
 });
 }
+
+
+let enteredIntegers = 0;
+function checkInput() {
+    const inputElement = document.getElementById('roll_num');
+
+    if (inputElement.value.length >= 0 && inputElement.value.length == 9) {
+        enteredIntegers = inputElement.value.length;
+        if (enteredIntegers === 9) {
+            fetch_data({'registration_number':inputElement.value,
+                        'admin_campus_resource':document.getElementById("location-selector").value})
+            var audio = new Audio('../static/beep.mp3');
+            audio.play();
+            inputElement.value = '';
+            enteredIntegers = 0;
+        } 
+    } 
+}
+
+setInterval(function () {
+    checkInput()
+}, 1000);
