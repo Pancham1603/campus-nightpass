f1c6f6ab7cc21ffcf2a07e0f8056defc5d8a5791
akarsh911 <akarsh91140@gmail.com>
2023-11-28 18:04:29 +0530

    try static

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR,"apps/users"))
 
 # Password validation
