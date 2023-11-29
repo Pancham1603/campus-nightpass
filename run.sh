f395afaf85dd50ab8b0dcf792ec9042773b934fd
Pancham Agarwal <pancham1603@gmail.com>
2023-11-29 12:37:58 +0530

    modifeid run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --clear
-yes
+python manage.py collectstatic --noinput
 gunicorn -b 0.0.0.0:4376 core.wsgi:application
