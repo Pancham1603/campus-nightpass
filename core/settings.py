b7ed2cdc24d6cb2c8036fe5dae29e8e160cd5bc8
Pancham Agarwal <pancham1603@gmail.com>
2023-11-29 16:22:18 +0530

    Update middleware in settings.py

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'core.middleware',
+    'core.middleware.RedirectUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'detect.middleware.UserAgentDetectionMiddleware',
