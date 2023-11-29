011a5e9aefff94ab2785a077c6cb6cf5e06582d7
Pancham Agarwal <pancham1603@gmail.com>
2023-11-29 12:32:12 +0530

    modified collectstatic in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
-yes
+python manage.py collectstatic --clear
 gunicorn -b 0.0.0.0:4376 core.wsgi:application
