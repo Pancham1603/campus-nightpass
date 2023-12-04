7e5af090391d3e13cd91f85d91ffa3014b52ac0d
Pancham Agarwal <pancham1603@gmail.com>
2023-12-04 18:49:50 +0530

    Fix NightPass filter in reset_nightpass.py

--- apps/users/management/commands/reset_nightpass.py
+++ apps/users/management/commands/reset_nightpass.py
@@ -7,5 +7,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Running your cron job...'))
-        NightPass.objects.filter(data=date.today()-timedelta(days=1)).update(valid=False)
+        NightPass.objects.filter(date=date.today()-timedelta(days=1)).update(valid=False)
         self.stdout.write(self.style.SUCCESS('Cron job completed successfully'))
