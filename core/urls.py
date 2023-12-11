4c7ae7745b56c8ebab0ca38b57b3ff73fd2ca709
Pancham Agarwal <pancham1603@gmail.com>
2023-12-12 04:35:03 +0530

    minor bug fix test

--- core/urls.py
+++ core/urls.py
@@ -17,17 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-# from apps.users import views as user_views
+from apps.users import views as user_views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-#    path('', include("apps.nightpass.urls")),
-#    path('admin/', admin.site.urls),
-#    path('admin/login', user_views.login_user),
-#    path('login/', user_views.login_user),
-#    path('logout/', user_views.logout_user),
-#    path('users/', include("django.contrib.auth.urls")),
-#    path('accounts/google/login/callback/', user_views.oauth_callback),
-#    path('access/', include("apps.validation.urls")),
-#    path('hijack/', include('hijack.urls')),
+   path('', include("apps.nightpass.urls")),
+   path('admin/', admin.site.urls),
+   path('admin/login', user_views.login_user),
+   path('login/', user_views.login_user),
+   path('logout/', user_views.logout_user),
+   path('users/', include("django.contrib.auth.urls")),
+   path('accounts/google/login/callback/', user_views.oauth_callback),
+   path('access/', include("apps.validation.urls")),
+   path('hijack/', include('hijack.urls')),
    ]
