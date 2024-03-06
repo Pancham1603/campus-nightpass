f578e9cc56d44e373d658a087b65474dd2d0ea23
Pancham Agarwal <pancham1603@gmail.com>
2024-03-06 19:39:55 +0530

    rollback redirect middleware

--- core/middleware.py
+++ core/middleware.py
@@ -9,8 +9,10 @@ class RedirectUserMiddleware:
         # Check if the user is authenticated and if their username is a specific value
         if (
             request.user.is_authenticated
-            and request.user.user_type == 'security' and request.path not in ['/access', '/access/', '/logout', '/logout/', '/hostel', '/hostel/']
+            and request.user.user_type == 'security'
+            and not request.session.get('redirected', False)
         ):
+            request.session['redirected'] = True
             return redirect('/access')
 
         # Continue with the normal request/response cycle
